ba0ddf8f093d63b9e6ef160c983b4ab214cb401e
Gabriel Puello <gabpuello@gmail.com>
2015-02-27 09:38:43 -0800

    completed spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require("rspec")
+require("stylist")
+require("client")
+require("pg")
+
+
+
+DB = PG.connect({:dbname => "hair_salon"})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM stylists *;")
+    DB.exec("DELETE FROM clients *;")
+  end
+end
