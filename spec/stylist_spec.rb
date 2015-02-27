28a6ae385d72c0a0ae77237e8ef2af26545dbfbd
Gabriel Puello <gabpuello@gmail.com>
2015-02-27 10:24:29 -0800

    add all and save methods

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -8,4 +8,26 @@ describe(Stylist) do
       expect(test_stylist.name()).to(eq("Manuel"))
     end
   end
+
+  describe("#id") do
+    it("returns it ID") do
+      test_stylist = Stylist.new({:name => "Manuel", :id => nil})
+      expect(test_stylist.id()).to(eq(nil))
+    end
+  end
+
+  describe(".all") do
+    it("returns all stylists") do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('adds a stylist to an array of stylists') do
+      test_stylist = Stylist.new(:name => "Manuel", :id => nil)
+      test_stylist.save()
+      expect(Stylist.all()).to(eq([test_stylist]))
+    end
+  end
+
 end
